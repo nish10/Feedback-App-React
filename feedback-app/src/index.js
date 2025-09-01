dc1321d22320fba7852ad3e1b8dbeb86e9d594b7
Nishant <nishant.raj334@gmail.com>
2025-09-01 09:24:52 +0530

    create react app and build first app component

---
+++ feedback-app/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client' // updated import statement for React 18
+import App from './App'
+import './index.css'
+
+const root = ReactDOM.createRoot(document.getElementById('root')); // updated for React 18
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+); // updated render method for React 18
+
