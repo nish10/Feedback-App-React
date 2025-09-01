d08bd52e0f33576f8803e5f3a2b338c51d7aa9e0
Nishant <nishant.raj334@gmail.com>
2025-09-01 17:50:49 +0530

    dynamic vlaues and lists in jsx

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -2,9 +2,25 @@ import React from 'react'
 
 
 function App() {
+  const title = 'Blog Post'
+  const body = 'This is my blog'
+  const comments = [
+    {id: 1, text: 'Comment one'},
+    {id: 2, text: 'Comment two'},
+    {id: 3, text: 'Comment three'}, 
+  ]
   return (
     <div className='container'>
-      <h1>My Feedback App</h1>
+      <h1>{title.toUpperCase() }</h1>
+      <p>{body}</p>
+      <div className="comments">
+        <h3>Comments {comments.length}</h3>
+        <ul>
+            {comments.map((comment, index) => {
+              return (<li key={index}>{comment.text}</li>)
+            })}
+        </ul>
+      </div>
     </div>
   )
 }
@@ -12,7 +28,6 @@ function App() {
 export default App
 
 // Alternative way with <JSX></JSX>
-
 
 // function App() {
 //   return React.createElement('div', {className: 'container'}, 
