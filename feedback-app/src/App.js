0c9b4238f88dbe2186b8e5c86658b9314175bc0c
Nishant <nishant.raj334@gmail.com>
2025-09-02 16:52:26 +0530

    hader component added

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,37 +1,14 @@
 import React from 'react'
-
+import Header from './components/Header'
 
 function App() {
-  const title = 'Blog Post'
-  const body = 'This is my blog'
-  const comments = [
-    {id: 1, text: 'Comment one'},
-    {id: 2, text: 'Comment two'},
-    {id: 3, text: 'Comment three'}, 
-  ]
-  const loading = false
-  const showComments = true
-
-  if (loading) return <h1>Loading...</h1>
-
-  const commentBlocks = (
-        <div className="comments">
-        <h3>Comments {comments.length}</h3>
-        <ul>
-            {comments.map((comment, index) => {
-              return (<li key={index}>{comment.text}</li>)
-            })}
-        </ul>
-      </div>
-      )
-
   return (
-    <div className='container'>
-      <h1>{title.toUpperCase() }</h1>
-      <p>{body}</p>
-
-      {showComments && commentBlocks}
-    </div>
+    <>
+      <Header text='Hello World'/>
+      <div className='container'>
+        <h1>My Feedback App</h1>
+      </div>
+    </>
   )
 }
 
---
+++ feedback-app/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header(props) {
+  return (
+  <header>
+    <div className="container">
+      <h2>{props.text}</h2>
+    </div>
+  </header>
+  )
+}
+
+export default Header
