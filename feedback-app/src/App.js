f87f0e5b36f0d6d217e6f5bb9b62113b51359814
nishkuma <nishant.3.kumar@nokia.com>
2025-09-02 20:52:23 +0530

    porptypes defaultProps and downgraded react to 18 to make the deprecated work

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -4,7 +4,7 @@ import Header from './components/Header'
 function App() {
   return (
     <>
-      <Header text={30} />
+      <Header text='Feedback UI' />
       <div className='container'>
         <h1>My Feedback App</h1>
       </div>
@@ -13,12 +13,4 @@ function App() {
 }
 
 export default App
-
-// Alternative way with <JSX></JSX>
-
-// function App() {
-//   return React.createElement('div', {className: 'container'}, 
-//     React.createElement('h1', {}, 'My Feedback App')
-//   )
-// }
 
--- feedback-app/src/components/Header.jsx
+++ feedback-app/src/components/Header.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-function Header(props) {
+function Header({ text = 'Default Header' }) {
   return (
   <header>
     <div className="container">
-      <h2>{props.text}</h2>
+      <h2>{text}</h2>
     </div>
   </header>
   )
 }
 
+// Header.defaultProps = {
+//   text: 'Default Header'
+// }
+
 Header.propTypes = {
-  text: PropTypes.string
+  text: PropTypes.string.isRequired
 }
+
 
 export default Header
