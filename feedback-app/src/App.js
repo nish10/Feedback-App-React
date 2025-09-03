e3c3a9ac76121a1121068940c4c477ff05d2386d
nish10 <nishant.raj334@gmail.com>
2025-09-03 10:20:52 +0530

    card component and conditional styles

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
 import { useState } from 'react';
 import FeedbackData from './data/FeedbackData';
+import Card from './components/shared/Card';
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
