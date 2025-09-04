48329e92c4bf1187ae58642cf684ee8c698fe7a0
Nishant <nishant.raj334@gmail.com>
2025-09-04 18:05:39 +0530

    add feedback

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -5,6 +5,7 @@ import { useState } from 'react'
 import FeedbackData from './data/FeedbackData'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
+import { v4 as uuidv4 } from 'uuid'
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
@@ -15,11 +16,16 @@ function App() {
     }
   }
 
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4()
+    setFeedback([newFeedback, ...feedback])
+  }
+
   return (
     <>
       <Header bgColor="#1A1A2E" color="#ff6a95" text="Feedback UI" />
       <div className="container">
-        <FeedbackForm />
+        <FeedbackForm handleAdd={addFeedback} />
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
