1de230605535e64bfaa13233a5996ff95e0ccdfe
nish10 <nishant.raj334@gmail.com>
2025-09-07 16:12:03 +0530

    react router dom and about page

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -6,6 +6,10 @@ import FeedbackData from './data/FeedbackData'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
 import { v4 as uuidv4 } from 'uuid'
+import AboutPage from './pages/AboutPage'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import AboutIconLink from './components/AboutIconLink'
+
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
@@ -22,14 +26,22 @@ function App() {
   }
 
   return (
-    <>
+    <Router>
       <Header bgColor="#1A1A2E" color="#ff6a95" text="Feedback UI" />
       <div className="container">
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+        <Routes>
+          <Route exact path="/" element={
+            <>
+              <FeedbackForm handleAdd={addFeedback} />
+              <FeedbackStats feedback={feedback} />
+              <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+              <AboutIconLink />
+            </>
+          } />
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
       </div>
-    </>
+    </Router>
   )
 }
 
